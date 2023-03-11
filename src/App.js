b8770de74d7fbc091978e4ff3d0246be9ab80566
David Lambert <delambert12@gmail.com>
2023-03-11 17:03:23 -0600

    added custom logout context

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
-import AuthContext from './store/auth-context';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-/*useEffect only runs after everything is rerendered, and only if dependencies are met (and first time ran) */
-  useEffect (() => {
-    const checkLogin = localStorage.getItem('isLoggedIn')
+  /*useEffect only runs after everything is rerendered, and only if dependencies are met (and first time ran) */
+  useEffect(() => {
+    const checkLogin = localStorage.getItem("isLoggedIn");
 
-    if (checkLogin === '1'){
-      setIsLoggedIn(true)
+    if (checkLogin === "1") {
+      setIsLoggedIn(true);
     }
-  }, [])
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
@@ -24,23 +24,26 @@ function App() {
     /* localStorage is global and for the browser, not tied to react. app tab in dev tools under localstorage cookies
       In this case 1 = loggedin and 0 = not
     */
-    localStorage.setItem('isLoggedIn', '1')
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
-    localStorage.removeItem('1')
+    localStorage.removeItem("1");
     setIsLoggedIn(false);
   };
-//.Provider is component of AuthState
+  //.Provider is component of AuthState
+  //every component inside auth will be able to access logoutHandler
   return (
-      <AuthContext.Provider>
-        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-        <main>
-          {!isLoggedIn && <Login onLogin={loginHandler} />}
-          {isLoggedIn && <Home onLogout={logoutHandler} />}
-        </main>
-      </AuthContext.Provider>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}
+    >
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </AuthContext.Provider>
   );
 }
 
